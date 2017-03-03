f9431dc479b500d1447d18cc09f98b7276289857
USER <user@mymacpro.local>
2017-03-03 14:46:18 +0800

    Initial Commit

---
+++ CodeInitiOSUIControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeInitiOSUIControl
+//
+//  Created by mobile manager on 17/3/3.
+//  Copyright © 2017年 Changhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CodeInitiOSUIControl/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CodeInitiOSUIControl
+//
+//  Created by mobile manager on 17/3/3.
+//  Copyright © 2017年 Changhong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
